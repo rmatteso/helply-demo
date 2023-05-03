10ac09e20ece5ce19091502a1db94e414b5f424e
Robert Matteson <rmatteso@gmail.com>
2023-05-03 05:25:32 -0400

    nav refinements

--- js/navigator.js
+++ js/navigator.js
@@ -1,23 +1,42 @@
 function getTarget() {
     let here = window.location.href;
-    let destination = here.split('/');
-
-    destination = destination[destination.length-1];
+    let url = here.split('/');
+    
+    let index = url.indexOf('#');
+    // in case there's no url parameter at all
+    if (index == -1) {
+        return 'home';
+    }
 
     let target;
+    let secondary_target;
 
-    switch(destination) {
-        case "":
-            target = 'home';
-            break;
+    if (url.length == (index + 1)) {
+        console.log('home');
+        
+        target = url[index];
+    }else if (url.length == (index + 2)) {
+        console.log('top-level page');
+        
+        target = url[index + 1];
+    }else if (url.length == (index + 3)) {
+        console.log('secondary-level page');
+        
+        target = url[index + 1];
+        secondary_target = url[index + 2];
+    }
+
+    let destination;
+
+    switch(target) {
         case "#":
-            target = 'home';
+            destination = 'home';
             break;
         default:
-            target = destination;
+            destination = target;
             break
     }
 
-    return target;
+    return destination;
 }
 
